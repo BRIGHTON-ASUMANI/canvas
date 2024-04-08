c7007a2f59650f028591ff1ace8eb9872422c827
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2024-04-08 16:18:42 +0300

    add font icons and style shapes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/button-has-type */
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { useState, useRef, useEffect } from 'react';
 import {
-  Navbar, Nav, Container, Col, Row, Form, Button,
+  Navbar, Nav, Container, Col, Row, Form, Card, Dropdown,
 } from 'react-bootstrap';
 import {
   Stage, Layer, Text, Circle, Rect, RegularPolygon,
 } from 'react-konva';
-import './App.css'; // Import the CSS file for styling
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUndo, faRedo, faTrash } from '@fortawesome/free-solid-svg-icons';
+import './App.css';
 
 function App() {
   const stageRef = useRef(null);
   const layerRef = useRef(null);
   const [shapes, setShapes] = useState([]);
   const [selectedShapeIndex, setSelectedShapeIndex] = useState(null);
-  const [brandName, setBrandName] = useState('Blank diagram');
+  const [brandName, setBrandName] = useState('Name of the project');
   const [undoStack, setUndoStack] = useState([]);
   const [redoStack, setRedoStack] = useState([]);
 
@@ -107,6 +112,7 @@ function App() {
         fill: 'transparent', // Inside color
         stroke: 'red', // Border color
         text: '', // Initialize text property
+        strokeWidth: 5,
         fillText: 'white', // Add fillText property
         fontSize: 12, // Add fontSize property
       },
@@ -121,13 +127,14 @@ function App() {
     const rect = {
       type: 'rect',
       attrs: {
-        x: window.innerWidth / 2 - 50,
-        y: window.innerHeight / 2 - 25,
-        width: 100,
-        height: 50,
+        x: window.innerWidth / 2,
+        y: window.innerHeight / 2,
+        width: 150,
+        height: 100,
         fill: 'transparent', // Inside color
         stroke: 'blue', // Border color
-        text: '', // Initialize text property
+        strokeWidth: 5,
+        text: 'teststst', // Initialize text property
         fillText: 'white', // Add fillText property
         fontSize: 12, // Add fontSize property
       },
@@ -147,6 +154,7 @@ function App() {
         radius: 50,
         fill: 'transparent', // Inside color
         stroke: 'cyan', // Border color
+        strokeWidth: 5,
         rotation: 90,
         text: '', // Initialize text property
         fillText: 'white', // Add fillText property
@@ -224,38 +232,79 @@ light">
             placeholder="Enter brand name"
             value={brandName}
             onChange={(e) => setBrandName(e.target.value)}
+            style={{ fontWeight: 'bold', fontSize: '16px' }}
           />
         </Navbar.Brand>
         <Nav>
-          <Nav.Link onClick={() => handleUndo()}>Undo</Nav.Link>
-          <Nav.Link onClick={() => handleRedo()}>Redo</Nav.Link>
-          <Nav.Link onClick={() => handleDelete()}>Delete</Nav.Link>
-          {' '}
-          {/* Delete button */}
+          <Nav.Link onClick={handleUndo}>
+            <FontAwesomeIcon icon={faUndo} />
+&nbsp;
+            Undo
+          </Nav.Link>
+          <Nav.Link onClick={handleRedo}>
+            <FontAwesomeIcon icon={faRedo} />
+&nbsp;
+            Redo
+          </Nav.Link>
+          <Nav.Link onClick={() => handleDelete()}>
+            {' '}
+            <FontAwesomeIcon icon={faTrash} />
+&nbsp;
+            Delete
+          </Nav.Link>
+
         </Nav>
         <Nav className="mr-auto">
-          <Nav.Link>File</Nav.Link>
-          <Nav.Link>Edit</Nav.Link>
-          <Nav.Link>Select</Nav.Link>
-          <Nav.Link>View</Nav.Link>
-          <Nav.Link>Insert</Nav.Link>
+          <Dropdown>
+            <Dropdown.Toggle as={Nav.Link} id="file-dropdown">
+              File
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item
+                variant="primary"
+                onClick={handleDownload}
+              >
+                Download Project
+              </Dropdown.Item>
+              <Dropdown.Item>test</Dropdown.Item>
+              <Dropdown.Item>test</Dropdown.Item>
+              <Dropdown.Item>test</Dropdown.Item>
+              <Dropdown.Item>test</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          <Nav.Link disabled>Edit</Nav.Link>
+          <Nav.Link disabled>Select</Nav.Link>
+          <Nav.Link disabled>View</Nav.Link>
+          <Nav.Link disabled>Insert</Nav.Link>
         </Nav>
       </Navbar>
       <Container fluid>
         <Row>
-          {/* Sidebar */}
-          <Col md={2} style={{ backgroundColor: '#f0f0f0', padding: '10px' }}>
-            <Button onClick={addCircle}>Add Circle</Button>
-            <Button onClick={addRectangle}>Add Rectangle</Button>
-            <Button onClick={addDiamond}>Add Diamond</Button>
-            <Button onClick={addText}>Add Text</Button>
-            <Button variant="primary" onClick={handleDownload}>Download Screenshot</Button>
+          <Col md={2} className="sidebar">
+            <Card className="px-2 pt-2">
+              <h6>Add Shapes</h6>
+              <div className="shape-container">
+                <svg width="40" height="40" onClick={addCircle}>
+                  <circle stroke="black" strokeWidth="3" fill="transparent" cx="20" cy="20" r="18" />
+                </svg>
+                <svg width="40" height="40" onClick={addRectangle}>
+                  <rect stroke="black" strokeWidth="3" fill="transparent" x="2" y="2" width="46" height="26" />
+                </svg>
+                <svg width="40" height="40" onClick={addDiamond}>
+                  <polygon stroke="black" strokeWidth="3" fill="transparent" rotate={90} points="20,2 38,20 20,38 2,20" />
+                </svg>
+                <svg width="50" height="50" onClick={addText}>
+                  <text stroke="black" strokeWidth="3" x="10" y="30" fontSize="24" fontWeight="bold">T</text>
+                </svg>
 
-            {' '}
-            {/* Button to add text */}
+                <div onClick={() => handleDelete()}>
+                  <span>Delete shape</span>
+                  <FontAwesomeIcon icon={faTrash} />
+                </div>
+              </div>
+            </Card>
           </Col>
 
-          {/* Main Content */}
           <Col md={10}>
             <div id="container">
               <Stage
@@ -270,68 +319,29 @@ light">
                       return (
                         <React.Fragment key={index}>
                           {shape.type === 'circle' && (
-                          <>
-                            <Circle
-                              {...shape.attrs}
-                              draggable
-                              onDblClick={() => handleTextDblClick(index)}
-                              className={isSelected ? 'selected' : ''}
-                            />
-                            <Text
-                              text={shape.attrs.text}
-                              x={shape.attrs.x - 20} // Adjust position as needed
-                              y={shape.attrs.y - 10} // Adjust position as needed
-                              fill={shape.attrs.fillText}
-                              fontSize={shape.attrs.fontSize}
-                              visible={!!shape.attrs.text}
-                              width={60} // Set the width to limit text to 30 characters
-                              align="center" // Align text to center
-                              wrap="word" // Wrap text by word
-                            />
-                          </>
+                          <Circle
+                            {...shape.attrs}
+                            draggable
+                            onDblClick={() => handleTextDblClick(index)}
+                            className={isSelected ? 'selected' : ''}
+                          />
                           )}
                           {shape.type === 'rect' && (
-                          <>
-                            <Rect
-                              {...shape.attrs}
-                              draggable
-                              onDblClick={() => handleTextDblClick(index)}
-                              className={isSelected ? 'selected' : ''}
-                            />
-                            <Text
-                              text={shape.attrs.text}
-                              x={shape.attrs.x + 5} // Adjust position as needed
-                              y={shape.attrs.y + 20} // Adjust position as needed
-                              fill={shape.attrs.fillText}
-                              fontSize={shape.attrs.fontSize}
-                              visible={!!shape.attrs.text}
-                              width={60} // Set the width to limit text to 30 characters
-                              align="center" // Align text to center
-                              wrap="word" // Wrap text by word
-                            />
-                          </>
+                          <Rect
+                            {...shape.attrs}
+                            draggable
+                            onDblClick={() => handleTextDblClick(index)}
+                            className={isSelected ? 'selected' : ''}
+                          />
                           )}
                           {shape.type === 'diamond' && (
-                          <>
-                            <RegularPolygon
-                              {...shape.attrs}
-                              sides={4}
-                              draggable
-                              onDblClick={() => handleTextDblClick(index)}
-                              className={isSelected ? 'selected' : ''}
-                            />
-                            <Text
-                              text={shape.attrs.text}
-                              x={shape.attrs.x - 25} // Adjust position as needed
-                              y={shape.attrs.y - 10} // Adjust position as needed
-                              fill={shape.attrs.fillText}
-                              fontSize={shape.attrs.fontSize}
-                              visible={!!shape.attrs.text}
-                              width={60} // Set the width to limit text to 30 characters
-                              align="center" // Align text to center
-                              wrap="word" // Wrap text by word
-                            />
-                          </>
+                          <RegularPolygon
+                            {...shape.attrs}
+                            sides={4}
+                            draggable
+                            onDblClick={() => handleTextDblClick(index)}
+                            className={isSelected ? 'selected' : ''}
+                          />
                           )}
                           {shape.type === 'text' && (
                           <Text
@@ -339,6 +349,9 @@ light">
                             {...shape.attrs}
                             onDblClick={() => handleTextDblClick(index)}
                             className={isSelected ? 'selected' : ''}
+                            width={90} // Set the width to limit text to 30 characters
+                            align="center" // Align text to center
+                            wrap="word"
                           />
                           )}
                         </React.Fragment>
